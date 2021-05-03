492d2a648fdc3db0a64e3a8fce4d177e0bcc61db
NelliTurpeinen <71337997+NelliTurpeinen@users.noreply.github.com>
2021-05-03 14:34:39 +0300

    Add files via upload

--- chulodb.sql
+++ chulodb.sql
@@ -1,43 +1,112 @@
-INSERT INTO dish(iddish, name, price, origin) VALUES( 1, 'Chicken Sekuwa', '7.60€', 'Nepal');
+-- MySQL dump 10.13  Distrib 5.7.32, for osx10.12 (x86_64)
+--
+-- Host: localhost    Database: chulodb
+-- ------------------------------------------------------
+-- Server version	5.7.32
 
-INSERT INTO dish(iddish, name, price, origin) VALUES( 2, 'Nepalese Chicken Noodle Soup', '6.50€', 'Nepal');
-INSERT INTO dish(iddish, name, price, origin) VALUES( 3, 'Thakali Food', '9.30€', 'Nepal');
-INSERT INTO dish(iddish, name, price, origin) VALUES( 4, 'Yomari', '5.70€', 'Nepal');
-INSERT INTO dish(iddish, name, price, origin) VALUES( 5, 'Sel Roti', '5.05€', 'Nepal');
-INSERT INTO dish(iddish, name, price, origin) VALUES( 6, 'Kheer', '6.20€', 'Nepal');
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-INSERT INTO dish(iddish, name, price, origin) VALUES( 7, 'Momo', '5.70€', 'Nepal');
-INSERT INTO dish(iddish, name, price, origin) VALUES( 8, 'Dhindo Set', '8.00€', 'Nepal');
-INSERT INTO dish(iddish, name, price, origin) VALUES( 9, 'Choila', '6.15€', 'Nepal');
-INSERT INTO dish(iddish, name, price, origin) VALUES( 10, 'Kwati', '6.00€', 'Nepal');
-INSERT INTO dish(iddish, name, price, origin) VALUES( 11, 'Nepali Bara', '4.85€', 'Nepal');
-INSERT INTO dish(iddish, name, price, origin) VALUES( 12, 'Classic Nepali Taas', '6.82€', 'Nepal');
+--
+-- Table structure for table `customer`
+--
 
-INSERT INTO dish(iddish, name, price, origin) VALUES( 13, 'Salmon Soup', '5.80€', 'Finland');
-INSERT INTO dish(iddish, name, price, origin) VALUES( 14, 'Karelian Pies', '6.00€', 'Finland');
-INSERT INTO dish(iddish, name, price, origin) VALUES( 15, 'Spinach Soup', '5.10€', 'Finland');
-INSERT INTO dish(iddish, name, price, origin) VALUES( 16, 'Pyttipannu', '6.20€', 'Finland');
-INSERT INTO dish(iddish, name, price, origin) VALUES( 17, 'Sautéed Reindeer', '7.40€', 'Finland');
-INSERT INTO dish(iddish, name, price, origin) VALUES( 18, 'Pea Soup', '5.30€', 'Finland');
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customer` (
+  `idcustomer` int(11) NOT NULL,
+  `firstname` varchar(45) DEFAULT NULL,
+  `lastname` varchar(45) DEFAULT NULL,
+  `address` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idcustomer`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-UPDATE dish SET image='Chicken Sekuwa.jpg' WHERE iddish=1;
+--
+-- Dumping data for table `customer`
+--
 
-UPDATE dish SET image='Nepali-Thukpa.jpg' WHERE iddish=2;
-UPDATE dish SET image='Thakali-Khana_.jpg' WHERE iddish=3;
-UPDATE dish SET image='yomari.jpeg' WHERE iddish=4;
-UPDATE dish SET image='Sel Roti.jpg' WHERE iddish=5;
-UPDATE dish SET image='Khir-.jpg' WHERE iddish=6;
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
 
-UPDATE dish SET image='momo(dumpling).jpg' WHERE iddish=7;
-UPDATE dish SET image='Dhido-set.jpg' WHERE iddish=8;
-UPDATE dish SET image='choila.jpeg' WHERE iddish=9;
-UPDATE dish SET image='Kwati.jfif' WHERE iddish=10;
-UPDATE dish SET image='Nepali Bara.jpg' WHERE iddish=11;
-UPDATE dish SET image='tass-set.png' WHERE iddish=12;
+--
+-- Table structure for table `dish`
+--
 
-UPDATE dish SET image='valkoinen-kalakaitto.jpg' WHERE iddish=13;
-UPDATE dish SET image='FinnishPies_binary_.png' WHERE iddish=14;
-UPDATE dish SET image='pinaattikeitto-1.jpg' WHERE iddish=15;
-UPDATE dish SET image='pyttipannu.jpg' WHERE iddish=16;
-UPDATE dish SET image='poronkaristys.jpg' WHERE iddish=17;
-UPDATE dish SET image='hernekeitto.jpg' WHERE iddish=18;
+DROP TABLE IF EXISTS `dish`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dish` (
+  `iddish` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `price` varchar(45) DEFAULT NULL,
+  `origin` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`iddish`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dish`
+--
+
+LOCK TABLES `dish` WRITE;
+/*!40000 ALTER TABLE `dish` DISABLE KEYS */;
+INSERT INTO `dish` VALUES (1,'Chicken Sekuwa','7.60€','Nepal'),(2,'Nepalese Chicken Noodle Soup','6.50€','Nepal'),(3,'Thakali Food','9.30€','Nepal'),(4,'Yomari','5.70€','Nepal'),(5,'Sel Roti','5.05€','Nepal'),(6,'Kheer','6.20€','Nepal'),(7,'Momo','5.70€','Nepal'),(8,'Dhindo Set','8.00€','Nepal'),(9,'Choila','6.15€','Nepal'),(10,'Kwati','6.00€','Nepal'),(11,'Nepali Bara','4.85€','Nepal'),(12,'Classic Nepali Taas','6.82€','Nepal'),(13,'Salmon Soup','5.80€','Finland'),(14,'Karelian Pies','6.00€','Finland'),(15,'Spinach Soup','5.10€','Finland'),(16,'Pyttipannu','6.20€','Finland'),(17,'Sautéed Reindeer','7.40€','Finland'),(18,'Pea Soup','5.30€','Finland');
+/*!40000 ALTER TABLE `dish` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order`
+--
+
+DROP TABLE IF EXISTS `order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order` (
+  `idorder` int(11) NOT NULL,
+  `idcustomer` int(11) DEFAULT NULL,
+  `iddish` int(11) DEFAULT NULL,
+  `deliverytime` varchar(45) DEFAULT NULL,
+  `itemamount` int(11) DEFAULT NULL,
+  `totalprice` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idorder`),
+  KEY `customer_order_idx` (`idcustomer`),
+  KEY `order_dish_idx` (`iddish`),
+  CONSTRAINT `customer_order` FOREIGN KEY (`idcustomer`) REFERENCES `customer` (`idcustomer`) ON UPDATE CASCADE,
+  CONSTRAINT `order_dish` FOREIGN KEY (`iddish`) REFERENCES `dish` (`iddish`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order`
+--
+
+LOCK TABLES `order` WRITE;
+/*!40000 ALTER TABLE `order` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-04-24 15:07:55
